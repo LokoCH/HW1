129b5271b782858da36c9b4ecb083c66d49db3d0
Kirill Serov <lokoch@mail.ru>
2021-10-10 00:54:08 +0500

    Добавьте файлы проекта.

---
+++ HW1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число от 1 до 50: ");
+
+            var tmp = Console.ReadLine();
+            int n;                                                        // количество чисел в последовательности
+            while (int.TryParse(tmp, out n) == false || n < 1 || n > 50)  // проверка на правильность ввода
+            {
+                Console.Clear();
+                Console.WriteLine("Неправильный ввод");
+                tmp = Console.ReadLine();
+            }
+
+            int i = 0;                                                    // количество нечетных чисел в последовательности
+
+            while (n != 0)                                                // считаем количество нечетных чисел в последовательности
+            {
+                if (n % 2 != 0)
+                    ++i;
+                --n;
+            }
+
+            // если нечетных чисел четное количество, то вся последовательность четная
+            // если нечетных чисел НЕчетное количество, то вся последовательность НЕчетная
+            if (i % 2 == 0)
+                Console.WriteLine("black");
+            else
+                Console.WriteLine("grimy");
+
+        }
+    }
+}
